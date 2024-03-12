16d0c0df039ed56a58d16b6d19932f963eef4a15
LeonIllya <dd0733567@gmail.com>
2024-03-12 17:31:01 +0200

    fix branches

--- src/main/java/book/store/BookStoreApplication.java
+++ src/main/java/book/store/BookStoreApplication.java
@@ -2,7 +2,6 @@ package book.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -10,16 +9,4 @@ public class BookStoreApplication {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book fairyTale = new Book();
-            fairyTale.setId(1L);
-            fairyTale.setAuthor("Shevchenko");
-
-            bookService.save(fairyTale);
-
-            System.out.println(bookService.findAll());
-        };
-    }
 }
