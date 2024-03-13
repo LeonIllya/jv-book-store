756cd73e5d801b20a19d8b6fd95d5bc36a206560
LeonIllya <dd0733567@gmail.com>
2024-03-13 20:39:57 +0200

    add specification for book

---
+++ src/main/java/book/store/repository/book/BookSpecificationBuilder.java
@@ -0,0 +1,36 @@
+package book.store.repository.book;
+
+import book.store.dto.BookSearchParametersDto;
+import book.store.model.Book;
+import book.store.repository.SpecificationBuilder;
+import book.store.repository.SpecificationProviderManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
+
+    @Override
+    public Specification<Book> build(BookSearchParametersDto searchParameters) {
+        Specification<Book> spec = Specification.where(null);
+        if (searchParameters.authors() != null && searchParameters.authors().length > 0) {
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("author")
+                    .getSpecification(searchParameters.authors()));
+        }
+
+        if (searchParameters.isbn() != null && searchParameters.isbn().length > 0) {
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("isbn")
+                    .getSpecification(searchParameters.isbn()));
+        }
+
+        if (searchParameters.titles() != null && searchParameters.titles().length > 0) {
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("title")
+                    .getSpecification(searchParameters.titles()));
+        }
+
+        return spec;
+    }
+}
