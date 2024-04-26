fcf761819c1eed4f3af02450640b7fea8ca9bcdb
Illia Leonov <dd0733567@gmail.com>
2024-04-26 18:03:53 +0300

    Delete (#11)
    * add pagination and swagger
    
    * add user
    
    * fix mistakes
    
    * add security and roles for users
    
    * fix problems
    
    * fix mistakes
    
    * fix conflicts
    
    * add JWT token
    
    * add JWT token
    
    * add Category to the project
    
    * fsx mistakes
    
    * fix mistakes
    
    * delete Category model

--- src/main/java/book/store/model/Book.java
+++ src/main/java/book/store/model/Book.java
@@ -5,17 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.Set;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.apache.commons.lang3.builder.ToStringExclude;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -40,14 +34,6 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
-    @ToStringExclude
-    @ManyToMany
-    @JoinTable(
-            name = "books_categories",
-            joinColumns = @JoinColumn(name = "book_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id")
-    )
-    private Set<Category> categories = new HashSet<>();
 
     @Column(nullable = false)
     private boolean isDeleted = false;
--- src/main/java/book/store/repository/book/BookRepository.java
+++ src/main/java/book/store/repository/book/BookRepository.java
@@ -1,10 +1,9 @@
 package book.store.repository.book;
 
 import book.store.model.Book;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-    List<Book> findAllByCategoriesId(Long categoryId);
+
 }
