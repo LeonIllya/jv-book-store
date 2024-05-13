6bf6d8c51920f352f2cf0ae8ed2d7c40508ca3aa
LeonIllya <dd0733567@gmail.com>
2024-05-13 16:49:33 +0300

    add ShoppingCart and CartItem in project

---
+++ src/main/java/book/store/dto/cartitem/UpdateCartItemDto.java
@@ -0,0 +1,10 @@
+package book.store.dto.cartitem;
+
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+@Data
+public class UpdateCartItemDto {
+    @Min(1)
+    private int quantity;
+}
---
+++ src/main/java/book/store/mapper/CartItemMapper.java
@@ -0,0 +1,17 @@
+package book.store.mapper;
+
+import book.store.config.MapperConfig;
+import book.store.dto.cartitem.CartItemDto;
+import book.store.dto.cartitem.CartItemRequestDto;
+import book.store.model.CartItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface CartItemMapper {
+    @Mapping(source = "book.id", target = "bookId")
+    @Mapping(source = "book.title", target = "bookTitle")
+    CartItemDto toDto(CartItem cartItem);
+
+    CartItem toModel(CartItemRequestDto requestDto);
+}
