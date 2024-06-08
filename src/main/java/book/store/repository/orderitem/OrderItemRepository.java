96f264e2b11c560a17f6e1bd26007a25faa22c41
LeonIllya <dd0733567@gmail.com>
2024-06-08 14:05:23 +0300

    improve my solution

--- src/main/java/book/store/repository/orderitem/OrderItemRepository.java
+++ src/main/java/book/store/repository/orderitem/OrderItemRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     @Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.order o "
-            + "WHERE o.id = :orderId AND oi.id = :orderItemId")
-    Optional<OrderItem> findByOrderIdAndOrderItemId(Long orderId, Long orderItemId);
+            + "WHERE o.id = :orderId AND o.user.id = :userId AND oi.id = :orderItemId ")
+    Optional<OrderItem> findByOrderIdAndOrderItemId(Long orderId, Long orderItemId, Long userId);
 }
