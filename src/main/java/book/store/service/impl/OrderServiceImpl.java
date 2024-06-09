c3b719ecf4212d3fd07b6b229e734bbabe84504b
Illia Leonov <dd0733567@gmail.com>
2024-06-09 12:30:19 +0300

    Develop (#18)
    * add pagination and swagger
    
    * add user
    
    * fix mistakes
    
    * add security and roles for users
    
    * fix problems
    
    * fix mistakes
    
    * fix conflicts
    
    * add JWT token
    
    * add JWT token
    
    * add Category to the project
    
    * fsx mistakes
    
    * fix mistakes
    
    * add ShoppingCart and CartItem
    
    * improve my solution,fix cart item and shopping cart
    
    * fix my code
    
    * fix my code
    
    * fix mistakes
    
    * problem with OrderItem model
    
    * add Order and OrderItem
    
    * improve my solution
    
    * improve my solution
    
    * improve my solution
    
    * improve my solution
    
    * improve my solution
    
    * improve my solution

--- src/main/java/book/store/service/impl/OrderServiceImpl.java
+++ src/main/java/book/store/service/impl/OrderServiceImpl.java
@@ -80,7 +80,7 @@ public class OrderServiceImpl implements OrderService {
                     userId)
                 .orElseThrow(() -> new EntityNotFoundException(
                     "Can`t find an order item by id: " + itemId));
-      
+
         return orderItemMapper.toDto(orderItem);
     }
 
