63aae794062dfb880f877a09a32e9a5e0533380a
LeonIllya <dd0733567@gmail.com>
2024-07-02 17:40:40 +0300

    fix update method in BookServiceImpl class

--- src/main/java/book/store/service/impl/BookServiceImpl.java
+++ src/main/java/book/store/service/impl/BookServiceImpl.java
@@ -48,9 +48,18 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto updateById(Long id, CreateBookRequestDto requestDto) {
-        Book update = bookMapper.toModel(requestDto);
-        update.setId(id);
-        return bookMapper.toDto(bookRepository.save(update));
+        Book book = bookRepository.findById(id).orElseThrow(()
+            -> new EntityNotFoundException("Can't find a book by id: " + id));
+
+        book.setId(id);
+        book.setAuthor(requestDto.getAuthor());
+        book.setCoverImage(requestDto.getCoverImage());
+        book.setDescription(requestDto.getDescription());
+        book.setIsbn(requestDto.getIsbn());
+        book.setPrice(requestDto.getPrice());
+        book.setTitle(requestDto.getTitle());
+
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
